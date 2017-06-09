cbc633c788a93b6452b3280168633568e1811dbd
Andrew Seidl <dev@aas.io>
2017-06-09 09:00:15 -0700

    Fix gzip + hardlink error
    Sometimes see the following error when extracting the sample data:
    
        gzip: filename has n other links -- unchanged
    
    Older versions of gzip seem to not like hardlinks, which Docker's
    default storage driver uses to minimize disk usage. Adding `-f` to force
    extraction and prevent the error.
    
    Also adding `-k` keep to allow the script to be rerun.

--- notebook-demo-docker/demo/packages/utils/create_ipums_easy.sh
+++ notebook-demo-docker/demo/packages/utils/create_ipums_easy.sh
@@ -3,5 +3,5 @@ set -e
 cd ~/pygdf/notebooks
 cat ~/pygdf/notebooks/create_table_ipums_easy.txt | ~/mapd/bin/mapdql -p HyperInteractive
 # Import CSV
-gzip -d ipums_easy.csv.gz
+gzip -f -k -d ipums_easy.csv.gz
 echo "COPY ipums_easy FROM '~/pygdf/notebooks/ipums_easy.csv';" | ~/mapd/bin/mapdql -p HyperInteractive
